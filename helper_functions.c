793032782bbf1c8a7e6e184ef9bd20bf8c3f9d45
Asser Elshafey <elshafeyasser@yahoo.com>
2023-04-18 11:12:55 -0700

    Adding main.h helper_functions.c _putchar.c

---
+++ helper_functions.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+}
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *ok = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+	return (ok);
+}
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int i, end;
+	char *array;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (end = 0; str[end] != '\0'; end++)
+	{
+	}
+
+	array = malloc(end + 1);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < end; i++)
+		array[i] = str[i];
+
+	return (array);
+}
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef simple_shell
+#define simple_shell
+
+
+/* helper_functions */
+int _putchar(char c);
+char *_strdup(char *str);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+
+#endif /* simple_shell */
