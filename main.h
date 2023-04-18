4e3e6dc435c85e198877583ed2b232e24146bb06
loaywaleed <loaywaleed9@gmail.com>
2023-04-18 11:38:58 -0700

    Added more function prototypes and added necessary includes

--- main.h
+++ main.h
@@ -1,12 +1,23 @@
 #ifndef simple_shell
 #define simple_shell
 
+/* Includes */
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
-/* helper_functions */
+/* Helper functions */
 int _putchar(char c);
 char *_strdup(char *str);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 
+/* Shell functions */
+int execute(char *cmd);
+char *get_cmd_path(char *command);
+char *prompt(void);
 
 #endif /* simple_shell */
