9f9b93f6aad1c791493db644e682981f1f6cf08f
Loay Waleed <loaywaleed9@gmail.com>
2023-04-21 12:07:48 -0700

    Added prototypes for our own strcnpy, strcmp, strncmp functions

--- _getenv.c
+++ _getenv.c
@@ -6,14 +6,14 @@
  * Return: NULL
  */
 
-char *_getenv(const char *name)
+char *_getenv(char *name)
 {
 	unsigned int i = 0;
 	char **env = environ;
 
 	while (env[i])
 	{
-		if (strncmp(name, env[i], strlen(name)) == 0)
+		if (_strncmp(name, env[i], strlen(name)) == 0)
 		{
 			return (&env[i][strlen(name) + 1]);
 		}
